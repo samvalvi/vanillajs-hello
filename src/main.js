3d30615e503521213f8724574017163ffd6e80fc
samvalvi <samuelvalerin@gmail.com>
2021-02-17 01:15:31 +0000

    final commit

--- src/main.js
+++ src/main.js
@@ -6,5 +6,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  //Chooose a random index
+  let firstWord = Math.floor(Math.random() * who.length + 0);
+  let secondWord = Math.floor(Math.random() * what.length + 0);
+  let thirdWord = Math.floor(Math.random() * when.length + 0);
+
+  let str =
+    who[firstWord] + " " + what[secondWord] + " " + when[thirdWord] + ".";
+  let sentence =
+    str
+      .toString()
+      .charAt(0)
+      .toLocaleUpperCase() + str.slice(1);
+
+  document.querySelector("#excuse").innerHTML = sentence;
 };
