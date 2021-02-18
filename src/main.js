2432a21a2df22a342b69fd6ff443d53a8fdcfeb8
samvalvi <samuelvalerin@gmail.com>
2021-02-18 17:39:06 +0000

    final commit

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   let who = ["the dog", "my granma", "his turtle", "my bird"];
   let what = ["eat", "pissed", "crushed", "broked"];
